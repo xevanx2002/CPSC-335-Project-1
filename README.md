2d8b27269699e3fd54799f94e6ec27494e2752d6
Evan <xevanx2002@csu.fullerton.edu>
2025-03-21 14:57:20 -0700

    Completion of Algorithm 2 including the write-up, psuedocode, and executable file for the program

---
+++ README.md
@@ -0,0 +1,6 @@
+# CPSC-355 Project 1
+## By: Ekaterina Miakotina & Evan Wenzel
+### Emails: kmiakotina@csu.fullerton.edu & xevanx2002@csu.fullerton.edu
+
+## How to Execute the Programs
+The two programs are created in differing languages, algorithm 1 in Python and algorithm 2 in C++. For algorithm 1 (INSERT INSTRUCTIONS HERE) For algorithm 2, run the submitted algo2.exe file that has been provided. This will execute the program within a created terminal, promting the user to input the necesary data for the program to execute the necesary steps and solve the problem.After solving the problem, the program will ask the user if they wish to start again and will loop back to the begining of the program.
--- algo2.cpp
+++ algo2.cpp
@@ -5,25 +5,21 @@ void greedyHamAlg(std::vector<int>& cityDis, std::vector<int>& totFuel, int mpg)
     int currentGas = 0;
     int startingCity = 0;
     
+    // Loop through the vector of cities to determine the best starting city within the circle
     for (int i = 0; i < cityDis.size(); i++) {
-        
         // Calculate the current available gas on the trip to determine trip viability
         currentGas = currentGas + (totFuel[i] * mpg) - cityDis[i];
 
-        if (currentGas < 0 || i != (cityDis.size() - 1)) {
+        // Checks the available gas after calculating travel for a negative number to see if the trip is possible
+        // Resets the trip while iterating through the possible starting cities
+        if (currentGas < 0 && i != (cityDis.size() - 1)) {
             // Reset the trip but starting from the next city instead
             startingCity = i + 1;
             currentGas = 0;
         };
     };
 
-    // If the total gas is below zero after calcs, there is no valid starting city 
-    if (currentGas >= 0) {
-        std::cout << "The preferred starting city for the sample provided is city " << startingCity << std::endl;
-    }
-    else {
-        std::cout << "No valid starting city was found" << std::endl;
-    }
+    std::cout << "The best starting city index is " << startingCity << std::endl;
 }
 
 int main() {
@@ -31,29 +27,36 @@ int main() {
     bool exit = false;
     char choice;
     
+    // Loop that iterates through the enitre program as long as the user want to
     while(exit != true) {
         std::cout << "Welcome to the Greedy Approach to Hamiltonian Problem Algorithm!" << std::endl;
+
+        // Askes the user for the desired number of cities to calulate through
         std::cout << "Please enter the number of cities you wish to travel!" << std::endl;
         std::cin >> cityNum;
-
+        // Asks the user for the miles per gallon the vehicle uses
         std::cout << "Please enter the miles per gallon for the vehicle!" << std::endl;
         std::cin >> mpg;
 
+        // CityDis is the distances between each city within an array while totFuel is the fuel available in each city
         std::vector<int> cityDis(cityNum);
         std::vector<int> totFuel(cityNum);
 
         std::cout << "Please enter the following distances between cities and the fuel fuel available at each city!" << std::endl;
         for (int i = 0; i < cityNum; i++) {
-            std::cout << "City Distance " << (i + 1) << ": ";
+            // Each iteration is for the distance between the starting city and the next
+            std::cout << "City Distance " << i << ": ";
             std::cin >> cityDis[i];
             std::cout << std::endl;
-            
-            std::cout << "Fuel Available for City " << (i + 1) << ": ";
+            // Each iteration is for the total fuel available in each city 
+            std::cout << "Fuel Available for City " << i << ": ";
             std::cin >> totFuel[i];
             std::cout << std::endl;
         };
 
+        // Run the algorithm with the inputed information provided
         greedyHamAlg(cityDis, totFuel, mpg);
+        // Ask the user if they want to run the algorithm again with different inputs
         std::cout << "Would you like to go again? Y | N" << std::endl;
         std::cin >> choice;
 
